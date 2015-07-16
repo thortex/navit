4256feee83c114fc80f816364399e7a9ae3488be
Pierre GRANDIN <grandinp@altern.org>
2015-07-15 20:11:08 -0700

    First attempt at automating internal gui tests

---
+++ ci/internal_gui_tests.sh
@@ -0,0 +1,8 @@
+# try to get to the town search
+sleep 1; xdotool key Return
+sleep 1; xdotool key Down
+sleep 1; xdotool key Return
+sleep 1; xdotool key Down
+sleep 1; xdotool key Down
+sleep 1; xdotool key Return
+import -window root $CIRCLE_ARTIFACTS/internal_gui_test.png
