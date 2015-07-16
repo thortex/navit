fd9759adacd4474ceb9c9397d2f5e86ceb373bfd
Pierre GRANDIN <grandinp@altern.org>
2015-07-15 20:42:49 -0700

    First attempt at gui_internal_search ui testing

--- ci/internal_gui_tests.sh
+++ ci/internal_gui_tests.sh
@@ -6,3 +6,6 @@ sleep 1; xdotool key Down
 sleep 1; xdotool key Down
 sleep 1; xdotool key Return
 import -window root $CIRCLE_ARTIFACTS/internal_gui_test.png
+sleep 1; xdotool key M
+sleep 1; xdotool key U
+import -window root $CIRCLE_ARTIFACTS/internal_gui_test_munchen.png
