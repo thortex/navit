35f3e58e44b87dbbd31ba0b416e714cd29953416
Michael Dankov <tryagain@navit-project.org>
2015-09-03 01:17:29 +0300

    typo

--- ci/update_version.sh
+++ ci/update_version.sh
@@ -1,7 +1,7 @@
 [ -n "$CIRCLE_BUILD_NUM" ] &&
    echo $(( 5658 + $CIRCLE_BUILD_NUM )) >version-int.txt && 
    git config user.email "navit@navit-project.org" &&
-   git config user.email "update_version.sh script" &&
+   git config user.name "update_version.sh script" &&
    git add version-int.txt && 
    git commit --no-edit -m "Update version, circle build $CIRCLE_BUILD_NUM" &&
    echo version-int.txt now contains `cat version-int.txt`
